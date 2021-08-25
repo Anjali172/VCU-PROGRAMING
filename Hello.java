0b0cf780ffad7cc65bdc184bfd6e746d3e94b392
Anjali172 <89546728+Anjali172@users.noreply.github.com>
2021-08-25 15:34:37 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*********************************************************************************
+ * Java Program Name
+**********************************************************************************
+* Student Name (Anjali and Reddy)
+* Wednesday August 25th
+* CMSC 225 and 004
+*********************************************************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("<Anjali>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Java/Lab 01/Running Command Line>");
+		System.out.println("<Wednesday August 25th>");
+		System.out.println("<CMSC 225 and 004>");
+		System.out.println();
+	}
+}
